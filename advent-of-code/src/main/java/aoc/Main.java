255b273eab1a157bc6c4b4cb208cff127b1c80d5
EDEFLON <elodie.deflon@gmail.com>
2023-10-10 10:55:38 +0200

    :tada: Modification de la structure de base du projet

---
+++ advent-of-code/src/main/java/aoc/Main.java
@@ -0,0 +1,13 @@
+package aoc;
+
+import aoc.year2016.day06.RepetitionCode;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // YEAR 2016 DAY 06
+        RepetitionCode rc = new RepetitionCode();
+        rc.recoverMessages("day_06.txt");
+    }
+}
