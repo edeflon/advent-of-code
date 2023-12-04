84d47c07bea5bc4953a71561c70c07e3f6fc352d
Elodie Deflon <elodie.deflon@gmail.com>
2023-12-04 20:11:27 +0100

    💩 Fixing part 2

--- advent-of-code/src/main/java/aoc/year2023/day04/Scratchcards.java
+++ advent-of-code/src/main/java/aoc/year2023/day04/Scratchcards.java
@@ -1,5 +1,7 @@
 package aoc.year2023.day04;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,14 +11,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class Scratchcards {
     public void totalPointsWorth(List<String> fileContent) {
         TreeSet<Card> cards = this.extractCards(fileContent);
-        int res = cards.stream()
+        int res1 = cards.stream()
                 .map(Card::score)
                 .mapToInt(Integer::intValue)
                 .sum();
-        System.out.println(res);
+        log.info("{}", res1);
 
         Map<Integer, Integer> numberOfCards = new HashMap<>();
         for (Card card: cards) {
@@ -26,15 +29,15 @@ public class Scratchcards {
         cards.forEach(card -> {
             int result = card.numberOfWinningNumbersDraw();
             int numberOfCardsToAdd = numberOfCards.get(card.getCardNumber());
-            for (int j = card.getCardNumber() + 1; j <= card.getCardNumber() + result && j < cards.size(); j++) {
+            for (int j = card.getCardNumber() + 1; j <= card.getCardNumber() + result && j <= cards.size(); j++) {
                 numberOfCards.put(j, numberOfCards.get(j) + numberOfCardsToAdd);
             }
         });
 
-        int superRes = numberOfCards.values().stream()
+        int res2 = numberOfCards.values().stream()
                 .mapToInt(Integer::intValue)
                 .sum();
-        System.out.println(superRes);
+        log.info("{}", res2);
     }
 
     private TreeSet<Card> extractCards(List<String> fileContent) {
