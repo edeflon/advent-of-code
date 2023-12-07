43ec6075b2aaf5f501f92369c89f4686eb398797
EDEFLON <elodie.deflon@geodis.com>
2023-12-07 22:52:32 +0100

    🍻 Correction if des enfers

--- advent-of-code/src/main/java/aoc/year2023/day07/CamelCards.java
+++ advent-of-code/src/main/java/aoc/year2023/day07/CamelCards.java
@@ -1,12 +1,12 @@
 package aoc.year2023.day07;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -63,89 +63,36 @@ public class CamelCards {
 
         for (Hand hand : hands) {
             Map<String, Integer> charCount = new HashMap<>();
-            List.of(hand.cards.split("")).forEach(chara -> charCount.put(chara, 0));
-            List.of(hand.cards.split("")).forEach(chara -> charCount.put(chara, charCount.get(chara) + 1));
+            List.of(hand.cards.split("")).forEach(chara -> charCount.put(chara, charCount.getOrDefault(chara, 0) + 1));
 
-            int jokerCount = charCount.entrySet().stream()
-                    .filter(chara -> Objects.equals(chara.getKey(), "J"))
-                    .map(Map.Entry::getValue)
+            int jokerCount = charCount.getOrDefault("J", 0);
+            charCount.put("J", 0);
+
+            int max = Collections.max(charCount.values()) + jokerCount;
+            int secondMax = charCount.values().stream()
+                    .sorted(Comparator.reverseOrder())
+                    .skip(1)
                     .findFirst()
                     .orElse(0);
 
-            if (jokerCount == 5) {
-                handByHandType.get(HandType.FIVE_OF_A_KIND).add(hand);
-                continue;
-            }
-
-            Optional<Integer> count5 = charCount.entrySet().stream()
-                    .filter(chara -> chara.getValue() == 5 && !Objects.equals(chara.getKey(), "J"))
-                    .map(Map.Entry::getValue)
-                    .findFirst();
-
-            if (count5.isPresent()) {
-                handByHandType.get(HandType.FIVE_OF_A_KIND).add(hand);
-                continue;
-            }
-
-            Optional<Integer> count4 = charCount.entrySet().stream()
-                    .filter(chara -> chara.getValue() == 4 && !Objects.equals(chara.getKey(), "J"))
-                    .map(Map.Entry::getValue)
-                    .findFirst();
-
-            if (count4.isPresent() && jokerCount == 1) {
-                handByHandType.get(HandType.FIVE_OF_A_KIND).add(hand);
-                continue;
-            } else if (count4.isPresent()) {
-                handByHandType.get(HandType.FOUR_OF_A_KIND).add(hand);
-                continue;
-            }
-
-            Optional<Integer> count3 = charCount.entrySet().stream()
-                    .filter(chara -> chara.getValue() == 3 && !Objects.equals(chara.getKey(), "J"))
-                    .map(Map.Entry::getValue)
-                    .findFirst();
-
-            long count2 = charCount.entrySet().stream()
-                    .filter(chara -> chara.getValue() == 2 && !Objects.equals(chara.getKey(), "J"))
-                    .map(Map.Entry::getValue)
-                    .count();
-
-            if (count3.isPresent() && jokerCount > 0) {
-                if (jokerCount == 2) {
-                    handByHandType.get(HandType.FIVE_OF_A_KIND).add(hand);
-                } else if (jokerCount == 1) {
-                    handByHandType.get(HandType.FOUR_OF_A_KIND).add(hand);
+            switch (max) {
+                case 5 -> handByHandType.get(HandType.FIVE_OF_A_KIND).add(hand);
+                case 4 -> handByHandType.get(HandType.FOUR_OF_A_KIND).add(hand);
+                case 3 -> {
+                    if (secondMax == 2) {
+                        handByHandType.get(HandType.FULL_HOUSE).add(hand);
+                    } else {
+                        handByHandType.get(HandType.THREE_OF_A_KIND).add(hand);
+                    }
                 }
-            } else if (count3.isPresent() && count2 == 1) {
-                handByHandType.get(HandType.FULL_HOUSE).add(hand);
-            } else if (count3.isPresent()) {
-                handByHandType.get(HandType.THREE_OF_A_KIND).add(hand);
-            } else if (count2 == 2 && jokerCount == 1) {
-                handByHandType.get(HandType.FULL_HOUSE).add(hand);
-            } else if (count2 == 2) {
-                handByHandType.get(HandType.TWO_PAIR).add(hand);
-            } else if (count2 == 1 && jokerCount > 0) {
-                if (jokerCount == 3) {
-                    handByHandType.get(HandType.FIVE_OF_A_KIND).add(hand);
-                } else if (jokerCount == 2) {
-                    handByHandType.get(HandType.FOUR_OF_A_KIND).add(hand);
-                } else if (jokerCount == 1) {
-                    handByHandType.get(HandType.THREE_OF_A_KIND).add(hand);
+                case 2 -> {
+                    if (secondMax == 2) {
+                        handByHandType.get(HandType.TWO_PAIR).add(hand);
+                    } else {
+                        handByHandType.get(HandType.ONE_PAIR).add(hand);
+                    }
                 }
-            } else if (count2 == 1) {
-                handByHandType.get(HandType.ONE_PAIR).add(hand);
-            } else if (jokerCount > 0) {
-                if (jokerCount == 4) {
-                    handByHandType.get(HandType.FIVE_OF_A_KIND).add(hand);
-                } else if (jokerCount == 3) {
-                    handByHandType.get(HandType.FOUR_OF_A_KIND).add(hand);
-                } else if (jokerCount == 2) {
-                    handByHandType.get(HandType.THREE_OF_A_KIND).add(hand);
-                } else if (jokerCount == 1) {
-                    handByHandType.get(HandType.ONE_PAIR).add(hand);
-                }
-            } else {
-                handByHandType.get(HandType.HIGH_CARD).add(hand);
+                default -> handByHandType.get(HandType.HIGH_CARD).add(hand);
             }
         }
 
