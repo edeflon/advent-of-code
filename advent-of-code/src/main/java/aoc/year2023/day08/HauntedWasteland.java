aa0f3660d4c50ec5588fe97ccbc4e59993ba600d
EDEFLON <elodie.deflon@geodis.com>
2023-12-08 18:46:58 +0100

    🚧 Day 8 part 2

--- advent-of-code/src/main/java/aoc/year2023/day08/HauntedWasteland.java
+++ advent-of-code/src/main/java/aoc/year2023/day08/HauntedWasteland.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class HauntedWasteland {
 
@@ -14,18 +15,30 @@ public class HauntedWasteland {
 
         Set<Node> nodes = this.extractNodes(fileContent);
 
+        Set<Node> travelNodes = nodes.stream()
+                .filter(node -> node.name.endsWith("A"))
+                .collect(Collectors.toSet());
         int steps = 0;
-        String target = "ZZZ";
-        String position = "AAA";
-        while (!target.equals(position)) {
-            String finalCurrentName = position;
-            Node currentNode = nodes.stream()
-                    .filter(node -> node.getName().equals(finalCurrentName))
-                    .findFirst()
-                    .orElseThrow();
-            position = currentNode.getCorrectDestination(instructions.get(steps % instructions.size()));
+        while (!travelNodes.stream().allMatch(node -> node.name.endsWith("Z"))) {
+            String instruction = instructions.get(steps % instructions.size());
+            travelNodes = travelNodes.stream()
+                    .map(node -> this.findNextNode(node, instruction, nodes))
+                    .collect(Collectors.toSet());
             steps++;
         }
+
+//        int steps = 0;
+//        String target = "ZZZ";
+//        String position = "AAA";
+//        while (!target.equals(position)) {
+//            String finalCurrentName = position;
+//            Node currentNode = nodes.stream()
+//                    .filter(node -> node.getName().equals(finalCurrentName))
+//                    .findFirst()
+//                    .orElseThrow();
+//            position = currentNode.getCorrectDestination(instructions.get(steps % instructions.size()));
+//            steps++;
+//        }
 
         System.out.println(steps);
     }
@@ -41,5 +54,13 @@ public class HauntedWasteland {
             }
         }
         return nodes;
+    }
+
+    private Node findNextNode(Node currentNode, String instruction, Set<Node> nodes) {
+        String next = currentNode.getCorrectDestination(instruction);
+        return nodes.stream()
+                .filter(nextNode -> nextNode.name.equals(next))
+                .findFirst()
+                .orElseThrow();
     }
 }
