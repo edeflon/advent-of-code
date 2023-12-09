96c28bbf329a34e680d1988ba0fc31dd2b6ad3aa
Elodie Deflon <elodie.deflon@gmail.com>
2023-12-09 20:14:41 +0100

    ✨ 9 2023 p1

--- advent-of-code/src/main/java/aoc/year2023/day09/MirageMaintenance.java
+++ advent-of-code/src/main/java/aoc/year2023/day09/MirageMaintenance.java
@@ -1,10 +1,66 @@
 package aoc.year2023.day09;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MirageMaintenance {
 
     public void sumOfExtrapolatedValues(List<String> fileContent) {
-        System.out.println("start");
+        List<List<Long>> values = this.extractValues(fileContent);
+
+        long sum = 0;
+        for (List<Long> value : values) {
+            List<List<Long>> sequences = new ArrayList<>();
+            sequences.add(value);
+
+            boolean cycleComplete = false;
+            int step = 0;
+            while (!cycleComplete) {
+                List<Long> sequence = this.calculateSequence(sequences.get(step));
+                sequences.add(sequence);
+                cycleComplete = sequence.stream().allMatch(number -> number == 0L);
+                step++;
+            }
+
+            long history = this.calculateHistory(sequences);
+            sum += history;
+        }
+
+        System.out.println(sum);
+    }
+
+    private List<List<Long>> extractValues(List<String> fileContent) {
+        List<List<Long>> values = new ArrayList<>();
+
+        for (String line: fileContent) {
+            Pattern pattern = Pattern.compile("(-)?(\\d+)");
+            Matcher matcher = pattern.matcher(line);
+
+            List<Long> history = new ArrayList<>();
+            while (matcher.find()) {
+                history.add(Long.valueOf(matcher.group()));
+            }
+            values.add(history);
+        }
+
+        return values;
+    }
+
+    private List<Long> calculateSequence(List<Long> previousSequence) {
+        List<Long> sequence = new ArrayList<>();
+        for (int j = 0; j < previousSequence.size() - 1; j++) {
+            sequence.add(previousSequence.get(j + 1) - previousSequence.get(j));
+        }
+        return sequence;
+    }
+
+    private Long calculateHistory(List<List<Long>> sequences) {
+        long sum = 0;
+        for (int i = sequences.size() - 2; i >= 0; i--) {
+            sum += sequences.get(i).get(sequences.get(i).size() - 1);
+        }
+        return sum;
     }
 }
