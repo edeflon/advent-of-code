11b18799f75371b2079bf2a80f6cf2d2bd4a6fec
Elodie Deflon <elodie.deflon@gmail.com>
2023-12-09 18:41:20 +0100

    ✨ day 8 part 2

--- advent-of-code/src/main/java/aoc/year2023/day08/HauntedWasteland.java
+++ advent-of-code/src/main/java/aoc/year2023/day08/HauntedWasteland.java
@@ -1,8 +1,6 @@
 package aoc.year2023.day08;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -18,49 +16,54 @@ public class HauntedWasteland {
         Set<Node> travelNodes = nodes.stream()
                 .filter(node -> node.name.endsWith("A"))
                 .collect(Collectors.toSet());
-        int steps = 0;
-        while (!travelNodes.stream().allMatch(node -> node.name.endsWith("Z"))) {
-            String instruction = instructions.get(steps % instructions.size());
-            travelNodes = travelNodes.stream()
-                    .map(node -> this.findNextNode(node, instruction, nodes))
-                    .collect(Collectors.toSet());
-            steps++;
+        for (Node travelNode : travelNodes) {
+            long step = 0L;
+            String position = travelNode.getName();
+            while (!position.endsWith("Z")) {
+                String finalCurrentName = position;
+                Node currentNode = nodes.stream()
+                        .filter(node -> node.getName().equals(finalCurrentName))
+                        .findFirst()
+                        .orElseThrow();
+                position = currentNode.getCorrectDestination(instructions.get((int) (step % instructions.size())));
+                step++;
+            }
+            travelNode.steps = step;
         }
 
-//        int steps = 0;
-//        String target = "ZZZ";
-//        String position = "AAA";
-//        while (!target.equals(position)) {
-//            String finalCurrentName = position;
-//            Node currentNode = nodes.stream()
-//                    .filter(node -> node.getName().equals(finalCurrentName))
-//                    .findFirst()
-//                    .orElseThrow();
-//            position = currentNode.getCorrectDestination(instructions.get(steps % instructions.size()));
-//            steps++;
-//        }
+        List<Long> steps = travelNodes.stream()
+                .map(Node::getSteps)
+                .toList();
 
-        System.out.println(steps);
+        long lcm = this.lcmAll(steps);
+        System.out.println(lcm);
     }
 
     private Set<Node> extractNodes(List<String> fileContent) {
         Pattern pattern = Pattern.compile("(?<name>([A-Z]{3}))\\s+=\\s+\\((?<left>([A-Z]{3})), (?<right>([A-Z]{3}))\\)");
 
         Set<Node> nodes = new HashSet<>();
-        for (String line: fileContent) {
+        for (String line : fileContent) {
             Matcher matcher = pattern.matcher(line);
             if (matcher.find()) {
-                nodes.add(new Node(matcher.group("name"), matcher.group("left"), matcher.group("right")));
+                nodes.add(new Node(matcher.group("name"), matcher.group("left"), matcher.group("right"), 0, new ArrayList<>()));
             }
         }
         return nodes;
     }
 
-    private Node findNextNode(Node currentNode, String instruction, Set<Node> nodes) {
-        String next = currentNode.getCorrectDestination(instruction);
-        return nodes.stream()
-                .filter(nextNode -> nextNode.name.equals(next))
-                .findFirst()
-                .orElseThrow();
+    // Calculate greatest common divisor
+    private long gcd(long a, long b) {
+        return (b == 0 ? a : gcd(b, a % b));
+    }
+
+    // Calculate least common multiple
+    private long lcm(long a, long b) {
+        return (a * b) / this.gcd(a, b);
+    }
+
+    // Calculate least common multiple for every steps
+    private long lcmAll(List<Long> steps) {
+        return steps.stream().reduce(this::lcm).orElseThrow();
     }
 }
--- advent-of-code/src/main/java/aoc/year2023/day08/Node.java
+++ advent-of-code/src/main/java/aoc/year2023/day08/Node.java
@@ -3,12 +3,16 @@ package aoc.year2023.day08;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 public class Node {
     String name;
     String leftDestination;
     String rightDestination;
+    long steps;
+    List<Integer> multiples;
 
     public String getCorrectDestination(String instruction) {
         if ("R".equals(instruction)) {
