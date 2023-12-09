c744f5f68a3c1487a225ad2477891db62cee8d66
Elodie Deflon <elodie.deflon@gmail.com>
2023-12-09 18:54:10 +0100

    ✨ Init day 09 2023

---
+++ advent-of-code/src/main/java/aoc/year2023/day09/MirageMaintenance.java
@@ -0,0 +1,10 @@
+package aoc.year2023.day09;
+
+import java.util.List;
+
+public class MirageMaintenance {
+
+    public void sumOfExtrapolatedValues(List<String> fileContent) {
+        System.out.println("start");
+    }
+}
