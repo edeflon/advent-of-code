bde913a0fed17e6c998efe4602fa76325fd30086
Elodie Deflon <elodie.deflon@gmail.com>
2023-12-10 15:18:26 +0100

    ✨ init d10 2023

---
+++ advent-of-code/src/main/java/aoc/year2023/day10/PipeMaze.java
@@ -0,0 +1,10 @@
+package aoc.year2023.day10;
+
+import java.util.List;
+
+public class PipeMaze {
+
+    public void stepsToFarthestPoint(List<String> fileContent) {
+        System.out.println("start");
+    }
+}
